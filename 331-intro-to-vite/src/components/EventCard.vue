f92235213b2f7594f31031c7980cdcc330aec78c
Drew <bqian776@gmail.com>
2024-07-09 10:53:39 +0700

    #8.1/#8.2/#8.3/#8.4

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-// defineProps<{
-//   msg: string
-// }>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-})
+import Event from '@/types/Event'
+defineProps<{
+  event: Event
+}>()
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date: 'January 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee'
+// })
 </script>
 
 <template>
@@ -24,7 +24,6 @@ const event = ref({
     </div>
   </div>
 </template>
-
 <style scoped>
 .event-card {
   padding: 20px;
